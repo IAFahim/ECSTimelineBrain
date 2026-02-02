dd766d1c2943cac83b344ed2b9315cfb0069e9f7
vex.iafahim <iafahim.dev@gmail.com>
2026-02-02 21:10:28 +0600

    Delete Intents package and replace with new version
    - Removed old Intents package structure
    - Added new Intents package directory
    
    Co-Authored-By: Craft Agent <agents-noreply@craft.do>

--- Assets/Intents/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog
-
-All notable changes to the IAFahim Intents System package will be documented in this file.
--- Assets/Intents/Intent/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
-// Copyright (c) DefaultCompany. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Intent.Debug")]
-[assembly: InternalsVisibleTo("Intent.Editor")]
-[assembly: InternalsVisibleTo("Intent.Tests")]
