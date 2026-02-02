37f6f84fcbd5a3adf5350f83f6fdc25e30644587
vex.iafahim <iafahim.dev@gmail.com>
2026-02-02 14:18:18 +0600

    added Intent

--- Assets/Intents/Intent.Tests/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="I">
-// Copyright (c) I. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-using Unity.Entities;
-
-[assembly: DisableAutoCreation]
--- Assets/Intents/Intent/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="I">
-// Copyright (c) I. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Intent.Debug")]
-[assembly: InternalsVisibleTo("Intent.Editor")]
-[assembly: InternalsVisibleTo("Intent.Tests")]
