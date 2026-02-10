76ffc1b305148ea91d6ac3d20013417426fe07e0
vex.iafahim <iafahim.dev@gmail.com>
2026-02-10 13:57:02 +0600

    added Quil

--- Packages/com.bovinelabs.timeline/BovineLabs.Timeline/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="BovineLabs">
-//     Copyright (c) BovineLabs. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Debug")]
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Editor")]
-[assembly: InternalsVisibleTo("BovineLabs.Timeline.Tests")]
