35d96c674b19b28625652c65cc7c61715b0fa344
vex.iafahim <iafahim.dev@gmail.com>
2026-02-24 13:20:43 +0600

    init PlayerInputs

---
+++ Assets/Scripts/PlayerInputs/PlayerInputs/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
+// Copyright (c) DefaultCompany. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("PlayerInputs.Debug")]
+[assembly: InternalsVisibleTo("PlayerInputs.Editor")]
+[assembly: InternalsVisibleTo("PlayerInputs.Tests")]
